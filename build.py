33bf67b304087b4768b034e5bb39c44faf2f93aa
pedro.martins <pedro.martins@fabamaq.com>
2021-11-13 12:55:55 +0000

    Added a multiplatform python script to build the project

---
+++ build.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import os
+import sys
+from subprocess import call
+
+def cmake(*arguments):
+    """Runs a cmake command directly in os. Exits in case of error."""
+
+    ret_code = call(['cmake'] + list(arguments))
+
+    if ret_code != 0:
+        sys.exit(ret_code)
+
+
+def get_generator() -> str:
+    """Returns cmake generator based on platform."""
+
+    if sys.platform == 'linux' or sys.platform == 'linux2':
+        return 'Unix Makefiles'
+    if sys.platform == 'win32':
+        return 'Visual Studio 16 2019'
+    if sys.platform == 'darwin':
+        return 'Xcode'
+    sys.exit('Unknown platform')
+
+cmake(
+    '-B', 'build', '-G', get_generator()
+)
+
+sys.exit(0)
